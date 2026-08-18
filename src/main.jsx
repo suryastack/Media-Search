00178b1d4f6b4833146faaf7170ca8893294f734
Surya <suryajyotimantry3@gmail.com>
2026-08-18 16:59:45 +0530

    Fix router base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import {BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Media-Search"g>
         <App />
       </BrowserRouter>
     </Provider>
