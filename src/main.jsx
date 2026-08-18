b7e732a53583e4732633f88e34fd33fed001b327
Surya <suryajyotimantry3@gmail.com>
2026-08-18 17:04:06 +0530

    Fix React Router basename

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import {BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Media-Search"g>
+      <BrowserRouter basename="/Media-Search">
         <App />
       </BrowserRouter>
     </Provider>
